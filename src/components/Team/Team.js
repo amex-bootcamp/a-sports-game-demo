d6b7013480ebb302a8f46b7c4cc6257a04d14b2e
kchia <kchia87@gmail.com>
2021-06-16 14:24:24 -0400

    render static data within Team component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 export default class Team extends Component {
   render() {
-    return <div>This is the Team component</div>;
+    return (
+      <div>
+        <h2>Squirrel</h2>
+        <img
+          src="https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/raccoon.png"
+          alt=""
+        />
+        <p>Shots: 0</p>
+        <p>Score: 0</p>
+        <p>Shooting %: 0%</p>
+        <button>Shoot!</button>
+      </div>
+    );
   }
 }
