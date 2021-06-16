08ff80427f32a199bacc3c0761da230909f52b12
kchia <kchia87@gmail.com>
2021-06-16 14:51:49 -0400

    make squirrels and raccoons appear on the page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Game from "./components/Game/Game";
 import Team from "./components/Team/Team";
 import ScoreBoard from "./components/ScoreBoard/ScoreBoard";
 
+const raccoons = {
+  name: "Russiaville Raccoons",
+  logoSrc:
+    "https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/raccoon.png",
+};
+
+const squirrels = {
+  name: "Sheridan Squirrels",
+  logoSrc:
+    "https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/squirrel.png",
+};
+
 function App() {
   return (
     <div>
-      <Game />
-      <Team />
       <ScoreBoard />
+      <Game squirrels={squirrels} raccoons={raccoons} />
     </div>
   );
 }
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import Team from "../Team/Team";
 
 export default class Game extends Component {
   render() {
-    return <div>This is the Game component</div>;
+    return (
+      <div>
+        <Team
+          name={this.props.squirrels.name}
+          logoSrc={this.props.squirrels.logoSrc}
+        />
+        <div>
+          <p>Resets:</p>
+          <button>Reset Game</button>
+        </div>
+        <Team
+          name={this.props.raccoons.name}
+          logoSrc={this.props.raccoons.logoSrc}
+        />
+      </div>
+    );
   }
 }
