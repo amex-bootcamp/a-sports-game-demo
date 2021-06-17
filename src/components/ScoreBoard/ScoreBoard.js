e539f941246cd4d2d9835e20ab90bfbfdbe7497a
kchia <kchia87@gmail.com>
2021-06-17 11:51:31 -0400

    refactor: convert ScoreBoard into a functional component

--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class ScoreBoard extends Component {
-  render() {
-    return (
-      <section>
-        <div>
-          <h3>VISITORS</h3>
-          <h3>{this.props.visiting.score}</h3>
-        </div>
+export default function ScoreBoard(props) {
+  return (
+    <section>
+      <div>
+        <h3>VISITORS</h3>
+        <h3>{props.visiting.score}</h3>
+      </div>
 
-        <h3>SCOREBOARD</h3>
+      <h3>SCOREBOARD</h3>
 
-        <div>
-          <h3>HOME</h3>
-          <h3>{this.props.home.score}</h3>
-        </div>
-      </section>
-    );
-  }
+      <div>
+        <h3>HOME</h3>
+        <h3>{props.home.score}</h3>
+      </div>
+    </section>
+  );
 }
