796c2b3df3afaab41d51b5afb49a790fe7501511
kchia <kchia87@gmail.com>
2021-06-17 10:31:42 -0400

    add the shooting percentage

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -26,13 +26,27 @@ export default class Team extends Component {
   };
 
   render() {
+    // calculate the shooting % on each re-render
+    const shotPercentage = Math.round(
+      (this.state.score / this.state.shots) * 100
+    );
+
+    let shotPercentageElement;
+    if (this.state.shots > 0) {
+      shotPercentageElement = (
+        <div>
+          <p>Shooting %: {shotPercentage}</p>
+        </div>
+      );
+    }
+
     return (
       <div className="Team">
         <h2>{this.props.name}</h2>
         <img src={this.props.logoSrc} alt={this.props.name} />
         <p>Shots: {this.state.shots}</p>
         <p>Score: {this.state.score}</p>
-        <p>Shooting %: 0%</p>
+        {shotPercentageElement}
         <button onClick={this.handleShoot}>Shoot!</button>
       </div>
     );
