06444fe499531c1165e6295ce889d7271fb12961
kchia <kchia87@gmail.com>
2021-06-17 10:20:41 -0400

    handleShoot is working

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Team.css";
 
 export default class Team extends Component {
   constructor(props) {
@@ -10,15 +11,29 @@ export default class Team extends Component {
     };
   }
 
+  handleShoot = () => {
+    // make the shot 50% of the time only
+    // access the current score and current shots
+    this.setState((currentState) => {
+      return {
+        // also need to increment the shots by 1
+        shots: currentState.shots + 1,
+        // if the shot is successful, increment the score by 1
+        score:
+          Math.random() > 0.5 ? currentState.score + 1 : currentState.score,
+      };
+    });
+  };
+
   render() {
     return (
-      <div>
+      <div className="Team">
         <h2>{this.props.name}</h2>
         <img src={this.props.logoSrc} alt={this.props.name} />
         <p>Shots: {this.state.shots}</p>
         <p>Score: {this.state.score}</p>
         <p>Shooting %: 0%</p>
-        <button>Shoot!</button>
+        <button onClick={this.handleShoot}>Shoot!</button>
       </div>
     );
   }
